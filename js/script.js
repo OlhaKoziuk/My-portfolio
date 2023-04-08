4d916df6ac3a68d1bbd0d6ad22b072fa6626c08c
OlhaKozuik <gadyakaban@gmail.com>
2023-04-08 13:12:20 +0300

    starting css

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const links = document.querySelectorAll('nav a');
+function addClass() {
+    links.forEach(item => {
+        item.addEventListener('click', (e) => {
+            removeClass();
+            e.target.classList.add('active');
+        })
+    })
+}
+function removeClass() {
+    links.forEach(item => {
+        item.classList.remove('active');
+    })
+}
+
+addClass();
