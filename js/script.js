9194768abb02e60734a3a9b054aed4e3dce18df9
OlhaKozuik <gadyakaban@gmail.com>
2023-04-11 16:47:13 +0300

    added adaptive and js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 const links = document.querySelectorAll('nav a');
+const navItems = document.querySelectorAll('.header li');
+const burger = document.querySelector('.burger');
+const navList = document.querySelector('.header ul');
+const closeButton = document.querySelector('img[alt="close"]');
+
 function addClass() {
     links.forEach(item => {
         item.addEventListener('click', (e) => {
@@ -13,6 +18,36 @@ function removeClass() {
     })
 }
 
-addClass();
+function addBurger() {
+    if (window.innerWidth <= 600) {
+        burger.classList.remove('hidden');
+        navItems.forEach(item => {
+            item.classList.add('hidden');
+        });
+    };
+}
 
-;
+function showBurgerList() {
+    burger.addEventListener('click', (e) => {
+        navList.setAttribute('id', 'new-nav');
+        closeButton.classList.remove('hidden')
+        navItems.forEach(item => {
+            item.classList.remove('hidden');
+        });
+    });
+}
+
+function hideBurgerList() {
+    navList.addEventListener('click', () => {
+         navList.removeAttribute('id');
+        closeButton.classList.add('hidden')
+        navItems.forEach(item => {
+            item.classList.add('hidden');
+        });
+    })
+}
+
+addClass();
+addBurger();
+showBurgerList();
+hideBurgerList();
