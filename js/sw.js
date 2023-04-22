f9c2937d7699803112f545581f68bb00a4bc8d33
OlhaKozuik <gadyakaban@gmail.com>
2023-04-22 17:46:25 +0300

    add

--- js/sw.js
+++ js/sw.js
@@ -1,37 +1,62 @@
-let staticCache = 'portfolio'; 
-let dynamicCache = "portfolio-app";
+let staticCache = 'calculator-v5'; // створення назви кешу
+let dynamicCache = "d-app-v5";
+let assets = [ // створення списку з файлів, які будуть кешуватися
+    "/",
+    "/manifest.json",
+    "/index.html",
+    "/offline.html",
+    "/js/script.js",
+  
+];
 
 
+self.addEventListener("install", async e => { // інсталцяція server worker
+    let cache = await caches.open(staticCache); // відкриваємо наш кеш
+    await cache.addAll(assets); // записуємо нові файли у наш кеш
+});
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches
-            .open(staticCache)
-            .then((cache) => cache.addAll(["/index.html"]))
+self.addEventListener("activate", async e => { // активація server worker
+    let cache = await caches.keys(); // беремо всі кеші, які є на сайті
+
+    await Promise.all( // чекаємо, коли кожеш кеш завантажиться
+        cache // беремо ці кеші списком
+            .filter(cache_name => cache_name != staticCache) // створюємо список із кешів, які застралі
+            .filter(cache_name => cache_name != dynamicCache)
+            .map(cache_data => caches.delete(cache_data)) // видаляємо застарілі кеші по черзі
     );
 });
 
 
-self.addEventListener('activate', function (e) {
-    e.waitUntil(
-        caches.keys().then(function (cacheNames) {
-            return Promise.all(
-                cacheNames.filter(function (cacheName) {
-                    
-                }).map(function (cacheName) {
-                    return caches.delete(cacheName)
-                })
-            )
-        })
-    )
-})
+self.addEventListener("fetch", e => { // робимо запит на наш server worker (ось тут більшість коду та логіки буде створюватися)
+    
+    e.respondWith(checkCache(e.request));
 
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })   
-     )
+    e.respondWith(caches.match(e.request)); 
+    // робимо так, щоб наш сайт завантажувався з кешу, який ми знайшли по запуту
 });
 
 
+async function checkCache (request) {
+    const cache = await caches.match(request);
+    return cache ?? checkOnline(request);
+};
+
+async function checkOnline(request){
+    const cache = await caches.open(dynamicCache)
+
+    try{
+        const res = await fetch(request)
+        await cache.put(request, res.clone())
+        return res
+    }
+    catch(e){
+        const cachedRes = await cache.match(request);
+
+        if (cachedRes) {
+            return cachedRes;
+        } else {
+            return caches.match('./offline.html');
+        }
+        
+    }
+}
