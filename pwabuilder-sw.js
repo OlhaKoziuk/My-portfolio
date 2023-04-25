b26c8e8a94e2176691c46672b5f9416385f6aac5
OlhaKozuik <gadyakaban@gmail.com>
2023-04-25 15:18:05 +0300

    added fetch

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -10,6 +10,29 @@ self.addEventListener('install', event => {
         caches.open(cacheName)
             .then(cache => {
                 return cache.addAll(urlToCache);
+            })
+    )
+});
+
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (names) {
+            return Promise.all(
+                names.filter(function (name) {
+                    return name;
+                }).map(function (name) {
+                    return caches.delete(name);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.responWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request)
         })
     )
 })
+    
