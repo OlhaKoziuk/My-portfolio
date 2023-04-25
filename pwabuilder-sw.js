f692f5aac6be66f3abbb549a59b1734d1b5474f0
OlhaKozuik <gadyakaban@gmail.com>
2023-04-25 15:59:43 +0300

    added

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -5,22 +5,7 @@ const urlToCache = [
      './offline.html',
     './css/reset.css',
     './css/style.css',
-    './img/bootstrap.png',
-    './img/close.png',
-    './img/css.png',
-    './img/facebook.png',
-    './img/finsweet.png',
-    './img/git.png',
-    './img/html.png',
-    './img/insta.png',
-    './img/ITEA.png',
-    './img/javascript.png',
-    './img/linkedin.png',
     './img/my-photo.jpg',
-    './img/Prometheus-first.jpg',
-    './img/Prometheus-second',
-    './img/Star.png',
-    './img/white-star.png',
     './js/script.js'
 ];
 self.addEventListener('install', event => {
